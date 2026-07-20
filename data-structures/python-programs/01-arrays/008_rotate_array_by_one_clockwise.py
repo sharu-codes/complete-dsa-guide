86f8744483265a4f6daa46cc336a8154a7e6ddde
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-20 20:31:25 +0530

    Added 'Rotate an Array by One Element in Clockwise Position'

---
+++ data-structures/python-programs/01-arrays/008_rotate_array_by_one_clockwise.py
@@ -0,0 +1,10 @@
+# Given an array arr,
+# rotate the array by one position in clockwise direction.
+
+class Solution:
+    def rotate(self, arr):
+        last = arr[-1]
+        for i in range (len(arr) - 1, -1, -1):
+            arr[i] = arr[i-1]
+        arr[0] = last
+        return arr
