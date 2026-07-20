47b0fd9932e78cccf7c44e09d48fc8dc7632a820
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-20 20:27:49 +0530

    Added 'Subset of an Array' GeeksForGeeks

---
+++ data-structures/python-programs/01-arrays/006_subset_of_an_array.py
@@ -0,0 +1,21 @@
+# Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+
+class Solution:
+    #Function to check if a is a subset of b.
+    def isSubset(self, a, b):
+        # Your code here
+        
+        freq = {}
+        
+        for num in a:
+            if num in freq:
+                freq[num] += 1
+            else:
+                freq[num] = 1
+                
+                
+        for num in b:
+            if num not in freq or freq[num] == 0:
+                return False
+            freq[num] -= 1
+        return True
