2878eea1ee86a5901dd4d4c8270393fb3051b16f
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-21 12:30:53 +0530

    Added 'Find Element at a Given Index'

---
+++ data-structures/python-programs/01-arrays/018_find_element_at_given_index.py
@@ -0,0 +1,6 @@
+# Given an array arr[] of integers and an index i(0-based index). Return the element present at the index i in the array.
+
+class Solution:
+    def findElementAtIndex(self, i, arr):
+        # code here
+        return arr[i]
