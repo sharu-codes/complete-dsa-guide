737933a8f4af2a9d4f7da7c9d7358c0ccc9efb8a
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-27 20:01:01 +0530

    Added 'Sum of even Numbers in an Array'

---
+++ data-structures/java-programs/01-arrays/p005.java
@@ -0,0 +1,24 @@
+// sum of even numbers in an array
+
+import java.util.Scanner;
+
+public class p7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int sum = 0;
+
+        System.out.println("enter elements:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] % 2 == 0)
+                sum += arr[i];
+        }
+
+        System.out.println("sum of even numbers = " + sum);
+        sc.close();
+    }
+}
