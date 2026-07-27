d190b55b6c48c2396b094b331a6eadd555b64a48
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-27 20:04:41 +0530

    Added 'Reverse an Array in Java'

---
+++ data-structures/java-programs/01-arrays/p006.java
@@ -0,0 +1,31 @@
+// reverse an array in Java
+
+import java.util.*;
+
+public class p006 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("enter elements:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i=0;i<n/2;i++){
+            int temp = arr[i];
+            arr[i] = arr[n-1-i];
+            arr[n-1-i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
