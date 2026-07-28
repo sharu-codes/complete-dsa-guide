aaa1367fdaabf10fbfcb2b1987df4a163d603c0d
sharu-codes <pramanicksharanya2025@gmail.com>
2026-07-28 20:20:57 +0530

    Added 'Number of Prime Elements in an Array'

---
+++ data-structures/java-programs/02-matrix/p005.java
@@ -0,0 +1,38 @@
+// number of prime elements in an array
+
+import java.util.Scanner;
+
+public class p005 {
+
+    public static boolean isPrime(int num){
+        if(num <= 1)
+            return false;
+
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        int count = 0;
+
+        System.out.println("enter elements:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            if(isPrime(arr[i]))
+                count++;
+        }
+
+        System.out.println("number of prime elements = " + count);
+
+        sc.close();
+    }
+}
