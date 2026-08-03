d46e7b47c8a5fbec3fa4b18c481305107786edee
sharu-codes <pramanicksharanya2025@gmail.com>
2026-08-03 19:58:55 +0530

    Updated Java Problems

--- data-structures/java-programs/01-arrays/p005.java
+++ data-structures/java-programs/01-arrays/p005.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class p7 {
+public class p005 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter size: ");
