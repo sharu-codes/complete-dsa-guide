d81b315ab54e968bb7e7326902b98c7c91fa99e0
sharu-codes <pramanicksharanya2025@gmail.com>
2026-08-03 23:15:49 +0530

    Added 'Binary Search using Recursion'

---
+++ algorithms/01-searching-algorithms/c-programs/003_binary_search_with_recursion.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+void initialise (int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void display (int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+void bubble_sort (int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+int binary_search (int arr[], int low, int high, int key) {
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        else if (key < arr[mid]) {
+            return binary_search (arr, low, mid - 1, key);
+        }
+        else {
+            return binary_search (arr, mid + 1, high, key);
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n, key;
+    printf("enter the range: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter the elements: ");
+    initialise(arr, n);
+    printf("the array is: ");
+    display(arr, n);
+    bubble_sort(arr, n);
+    printf("\nenter the element to be searched: ");
+    scanf("%d", &key);
+    int result = binary_search(arr, 0, n-1, key);
+    if (result!=-1) {
+        printf("%d is found at %d", key, result);
+    }
+    else {
+        printf("%d is not found", key);
+    }
+}
