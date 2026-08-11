447f4d87609611207e571240e4f0d11dd2859b35
sharu-codes <pramanicksharanya2025@gmail.com>
2026-08-11 18:54:10 +0530

    Added Utility Function

---
+++ data-structures/01-basic/c-program/002_utility_function_gfg_2.c
@@ -0,0 +1,22 @@
+/*
+Given a number, you have to use if, else if, else conditional statements according to the following:
+if number is greater than 100: Print "Big" (without quotes)
+else if number is smaller than 10: Print "Small" (without quotes)
+else: Print "Number" (without quotes) 
+
+Note: Ensure that the output includes a newline after every print statement.
+
+*/
+
+void utility(int number) {
+    // Write the if, else if, else statements below
+    if (number > 100) {
+        printf("Big\n");
+    }
+    else if (number < 10) {
+        printf("Small\n");
+    }
+    else {
+        printf("Number\n");
+    }
+}
